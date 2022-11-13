89fd2241429a5c00cabf31ac6438ce604e999bfa
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-11-12 23:40:47 -0300

    chore: update main

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/screens/details_screen.dart';
-import 'package:todo_list/screens/task_list.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_list/app/view/home/home.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: TaskList());
+        home: Home());
   }
 }
