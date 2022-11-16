815163444916c45e631ea3f93a425cdd0f5e9268
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-11-16 04:42:36 -0300

    minor refactorings

--- todo_list/lib/app/repositories/task_repository.dart
+++ todo_list/lib/app/repositories/task_repository.dart
@@ -1,36 +1,29 @@
 import 'package:hive/hive.dart';
 
 import '../model/entities/hive_task_scheme.dart';
-import '../model/entities/task_entity.dart';
 
 class TaskRepository {
-  Box<HiveTaskScheme> box = Hive.box('todos');
+  Box<HiveTaskScheme> box = Hive.box('taskBox');
 
-  addTodo(HiveTaskScheme todo) {
-    //box = Hive.box('todos');
-
-    box.put(todo.title, todo);
+  addTodo(HiveTaskScheme todo) async {
+    await box.put(todo.title, todo);
   }
 
-  editTodo(String title, String newTitle) {
-    //final box = Hive.box('todos');
+  editTodo(String title, String newTitle) async {
     HiveTaskScheme temp = box.get(title)!;
     temp.title = newTitle;
-    box.put(title, temp);
+    await box.put(title, temp);
   }
 
-  void removeTodo(String title) {
-    //final box = Hive.box('todos');
-    box.delete(title);
+  void removeTodo(String title) async {
+    await box.delete(title);
   }
 
   getTodo(String title) {
-    final box = Hive.box('todos');
     return box.get(title);
   }
 
   get allTodos {
-    final box = Hive.box('todos');
     return box.values;
   }
 }
