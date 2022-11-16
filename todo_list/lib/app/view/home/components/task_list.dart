dc54606c3c59ea5f3db5a622be626e4cfa2a561c
Andrey-Matos <66771031+Andrey-Matos@users.noreply.github.com>
2022-11-15 23:45:21 -0300

    decouple task list

---
+++ todo_list/lib/app/view/home/components/task_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list/app/view/home/components/task_card.dart';
+
+import '../../../controller/task_provider.dart';
+
+class TaskList extends StatefulWidget {
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskProvider>(
+      builder: (context, provider, child) {
+        provider.fetch();
+        return ListView.builder(
+            itemCount: provider.displayValueSize,
+            itemBuilder: (context, i) => TaskCard(
+                title: provider.displayResults[i].title,
+                description: provider.displayResults[i].description,
+                dueDate: provider.displayResults[i].dueDate));
+      },
+    );
+  }
+}
